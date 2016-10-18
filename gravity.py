1145626bb466f703af35a5b8f31c2e45f683c5e4
chris <bertinato@ldeo.columbia.edu>
2016-10-18 17:33:35 -0400

    Added __init__ function for class.  Added function to read ZLS format files.

--- gravity.py
+++ gravity.py
@@ -5,6 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from ConfigParser import SafeConfigParser
+import datetime
 
 class Gravity:
 	# TO DO: Setup exceptions for this class.
@@ -32,28 +33,32 @@ class Gravity:
 	# Eccentricity squared
 	_e2 = np.float64(0.00669438002290)
 
-	_mGalToMS2 = np.float64(10e-5) # 1 mGal = 10^-5 m/s^2
+	_mGal = np.float64(10e-5) # 1 mGal = 10^-5 m/s^2
 
-	# Instance variables
+	_zls_file_format = {'line_name':10, 'gravity':8, 'spring_tension':8, \
+		'cross_coupling':7, 'raw_beam':8, 'vcc':8, 'al':8, 'ax':8, 've2':8, 'ax2':8, \
+		'xacc2':8, 'lacc2':8, 'xacc':8, 'lacc':8, 'par_port':8, 'platform_per':6}
 
-	# Conversion and calibration factors
-	self.eD = np.float64(1.11585e5) # ?
-	self.nD = np.float64(1.11369e5) # ?
+	def __init__(self):
 
-	self.meter_model = None
-	self.k_factor = None
-	self.pre_static_reading = None
-	self.post_static_reading = None
-	self.gravity_tie = None
+		# Conversion and calibration factors
+		self.eD = np.float64(1.11585e5) # ?
+		self.nD = np.float64(1.11369e5) # ?
 
-	# legacy
-	self.time_shift = None
-	self.filter_length = None
-	self.filter_type = None
+		self.meter_model = None
+		self.k_factor = None
+		self.pre_static_reading = None
+		self.post_static_reading = None
+		self.gravity_tie = None
+
+		# legacy
+		self.time_shift = None
+		self.filter_length = None
+		self.filter_type = None
 
 	################################
 
-	def read_meter_config(filename):
+	def read_meter_config(self, filepath):
 		errors = []
 
 		parser = SafeConfigParser()
@@ -85,14 +90,63 @@ class Gravity:
 		self.gravity_tie = parser.get('Survey', 'TieGravity')
 
 		if errors:
-    		raise Error(errors)
+			raise Error(errors)
 
-	def import_data_DGS_format(filename, filterdelay):
+	def read_ZLS_format_file(self, filepath):
+		col_list = ['datetime', 'gravity', 'spring_tension', \
+			'cross_coupling', 'raw_beam', 'vcc', 'al', 'ax', 've2', 'ax2', \
+			'xacc2', 'lacc2', 'xacc', 'lacc', 'par_port', 'platform_period']
 
-		print "Importing gravity data from %s." % filename
+		df = pd.DataFrame()
+
+		with open(filepath) as filehandle:
+			for index, line in enumerate(filehandle):
+				line_name = line[0:10].strip()
+
+				year = int(line[10:14])
+				jday = int(line[14:17])
+				h = int(line[17:19])
+				m = int(line[19:21])
+				s = int(line[21:23])
+
+
+				# TO DO: Check whether days are zero-padded.
+				# TO DO: Check whether day starts at 0 or 1.
+				dt = datetime.datetime(year, 1, 1) + \
+					datetime.timedelta(jday, seconds=s, minutes=m, hours=h)
+
+				gravity = np.float64(line[23:31])
+				st = np.float64(line[31:39])
+				cc = np.float64(line[39:46])
+				raw_beam = np.float64(line[46:54])
+				vcc = np.int32(line[54:61])
+				al = np.int32(line[62:69])
+				ax = np.int32(line[70:77])
+				ve2 = np.int32(line[78:85])
+				ax2 = np.int32(line[86:93])
+				xacc2 = np.int32(line[94:101])
+				lacc2 = np.int32(line[102:109])
+				xacc = np.int32(line[110:117])
+				lacc = np.int32(line[118:125])
+				par_port = line[126:134]
+				platform_period = np.int32(line[134:139])
+
+				row = pd.DataFrame([[dt, gravity, st, cc, raw_beam, vcc, al, \
+					ax, ve2, ax2, xacc2, lacc2, xacc, lacc, par_port, \
+					platform_period]], columns=col_list)
+
+				df = df.append(row)
+
+		df = df.set_index('datetime')
+		return df
+
+	def import_data_DGS_format(self, filepath, filterdelay):
+		# TO DO: Evaluate whether to apply filter delay here.
+		# TO DO: Is filter delay specified in meter config?
+		print "Importing gravity data from %s." % filepath
 
 		# Read data
-		df = pd.read_csv(filename)
+		df = pd.read_csv(filepath)
 
 		# Label columns
 		df.columns = ['QC_gravity','Gravity','Long_accel', 'Cross_accel', 'Beam', \
@@ -134,7 +188,7 @@ class Gravity:
 
 	################################
 
-	def import_pos(filename, interval=0):
+	def import_pos(self, filename, interval=0):
 
 		print "Importing trajectory data from %s." % filename
 
@@ -165,7 +219,7 @@ class Gravity:
 
 	################################
 
-	def join_grav_pos(df1, df2):
+	def join_grav_pos(self, df1, df2):
 
 		print "Combining data sets."
 
@@ -179,7 +233,7 @@ class Gravity:
 
 	################################
 
-	def filter_gravity(df, window):
+	def filter_gravity(self, df, window):
 
 		print "Filtering gravity with a window of %d seconds." % window
 
@@ -197,7 +251,7 @@ class Gravity:
 
 	################################
 
-	def plot_grav_qc(df, lines):
+	def plot_grav_qc(self, df, lines):
 
 		# setup pdf
 		pp = PdfPages('multipage.pdf')
@@ -333,11 +387,11 @@ class Gravity:
 
 	################################
 
-	def lever_arm_correction(lat, lon, height, pitch, roll, heading, dx, dy, dz):
+	def lever_arm_correction(self, lat, lon, height, pitch, roll, heading, dx, dy, dz):
 		# Returns corrected latitude, longitude, and height
 
-		dlam = (dl*sin(radians(heading)) + dx*cos(radians(heading))) / (eD*cos(lat))
-		dphi = (dl*cos(radians(heading)) - dx*sin(radians(heading))) / nD
+		dlam = (dl*sin(radians(heading)) + dx*cos(radians(heading))) / (_eD*cos(lat))
+		dphi = (dl*cos(radians(heading)) - dx*sin(radians(heading))) / _nD
 		dH = dh + dl*sin(radians(pitch)) + dx*sin(radians(roll))
 
 		lam = lon + dlam
@@ -348,13 +402,13 @@ class Gravity:
 
 	################################
 
-	def eotvos_correction(lat, lon, height):
+	def eotvos_correction(self, lat, lon, height):
 
 		# Radius of curvature of equatorial meridian
-		CN = a / (np.sqrt(1 - e2 * (np.sin(np.deg2rad(lat)))**2))
+		CN = _a / (np.sqrt(1 - _e2 * (np.sin(np.deg2rad(lat)))**2))
 
 		# Radius of curvature of prime meridian
-		CM = a * (1 - e2) / ((1 - e2 * (np.sin(np.deg2rad(lat)))**2)**(3/2))
+		CM = a * (1 - _e2) / ((1 - _e2 * (np.sin(np.deg2rad(lat)))**2)**(3/2))
 
 		# Easting velocity
 		VE = (CN + height)*np.cos(np.deg2rad(lat))*np.gradient(lon)
@@ -362,30 +416,30 @@ class Gravity:
 		# Northing velocity
 		VN = (CM + height)*np.gradient(lat)
 
-		eotvos = (VN**2 / a) * (1 - height / a + f * (2 - 3 * (np.sin(np.deg2rad(lat)))**2)) + \
-		(VE**2 / a) * (1 - height/a - f * (np.sin(np.deg2rad(lat)))**2) + \
+		eotvos = (VN**2 / _a) * (1 - height / _a + _f * (2 - 3 * (np.sin(np.deg2rad(lat)))**2)) + \
+		(VE**2 / _a) * (1 - height/_a - _f * (np.sin(np.deg2rad(lat)))**2) + \
 		2 * w * VE * np.cos(np.deg2rad(lat))
 
-		return eotvos * mGal
+		return eotvos * _mGal
 
 	################################
 
-	def lat_correction(lat):
+	def lat_correction(self, lat):
 		return -9.7803267715 * ((1 + 0.00193185138639*(np.sin(np.deg2rad(lat)))**2) \
-		/ np.sqrt(1 - 0.00669437999013*(np.sin(np.deg2rad(lat)))**2)) * mGal
+		/ np.sqrt(1 - 0.00669437999013*(np.sin(np.deg2rad(lat)))**2)) * _mGal
 
 	################################
 
-	def free_air_correction(height):
-		return 0.3086 * height * mGal
+	def free_air_correction(self, height):
+		return 0.3086 * height * _mGal
 
 	################################
 
-	def vert_accel_correction(height):
+	def vert_accel_correction(self, height):
 	# From SciPy.org documentation:
 	# 	"The gradient is computed using second order accurate central differences in the
 	#	 interior and either first differences or second order accurate one-sides
 	#	 (forward or backwards) differences at the boundaries. The returned gradient hence
 	#	 has the same shape as the input array."
 
-		return pd.Series(np.gradient(np.gradient(height)), index=height.index) * mGal
+		return pd.Series(np.gradient(np.gradient(height)), index=height.index) * _mGal
