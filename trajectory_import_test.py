2fdd0886a7e09428b4ac25d30032e9eeb8e7e16d
chris <bertinato@ldeo.columbia.edu>
2017-03-02 17:45:09 -0500

    Changed trajectory file for trajectory import test to one that matches the test gravity data.

--- trajectory_import_test.py
+++ trajectory_import_test.py
@@ -1,8 +1,9 @@
 import gravity as g
 
-filepath = '/Users/chrisbert/Documents/Git/GDPS/sample_data/DGS/sample_trajectory.txt'
+filepath = '/Users/chrisbert/Documents/Git/GDPS/sample_data/AN03_F1007_20161130_iMAR_PrelimFinal_STD.txt'
 
 traj_test = g.Gravity()
-traj_test.import_trajectory(filepath, interval=.1, interp=True)
+#traj_test.import_trajectory(filepath, interval=.1, interp=True)
+traj_test.import_trajectory(filepath, interval=.1)
 
 print traj_test.trajectory
