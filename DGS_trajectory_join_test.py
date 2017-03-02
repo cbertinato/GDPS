bee2a7a71d33286aebcaf5ac7c6d0e3a7e42e50f
chris <bertinato@ldeo.columbia.edu>
2017-03-02 17:39:06 -0500

    Changed trajectory file for the join test to one that matches the test gravity data.

--- DGS_trajectory_join_test.py
+++ DGS_trajectory_join_test.py
@@ -1,14 +1,18 @@
 import gravity as g
 
 gravity_path = '/Users/chrisbert/Documents/Git/GDPS/sample_data/DGS/dgs_test_data.dat'
-trajectory_path = '/Users/chrisbert/Documents/Git/GDPS/sample_data/DGS/sample_trajectory.txt'
+trajectory_path = '/Users/chrisbert/Documents/Git/GDPS/sample_data/AN03_F1007_20161130_iMAR_PrelimFinal_STD.txt'
 
 data = g.Gravity()
 print 'Importing gravity data.'
 data.import_DGS_format_data(gravity_path)
 
+print data.gravity
+
 print 'Importing trajectory data.'
-data.import_trajectory(trajectory_path)
+data.import_trajectory(trajectory_path, interval=.1)
+
+print data.trajectory
 
 print 'Joining trajectory with gravity data.'
 data.join_grav_traj()
