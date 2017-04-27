17d7c56d765341c4b29b2ede28385c959bc8a4d6
chris <bertinato@ldeo.columbia.edu>
2017-04-27 16:22:32 -0400

    Renamed time module to timeutils to avoid conflict with module of the same name.

--- timeutils.py
+++ timeutils.py
@@ -1,7 +1,21 @@
-def convert_to_utc(wk, sow):
-  dt = datetime.datetime(1980,1,6) + datetime.timedelta(weeks=wk)
+def convert_to_utc(sow, **kwargs):
+    if 'week' in kwargs:
+        dt = datetime.datetime(1980,1,6) + datetime.timedelta(weeks=kwargs['week'])
+    elif 'date' in kwargs:
+        if isinstance(kwargs['date'], basestring):
+            d = kwargs['date'].split('-')
+            if len(d) != 3:
+                d = kwargs['date'].split('/')
+                if len(d) != 3:
+                    print 'date not correctly formatted. expect MM-DD-YYYY or MM/DD/YYYY'
+                    return
 
-  ls_table = [(1980,1,1,1981,7,1),\
+            dt = datetime.datetime(d[2], d[0], d[1])
+    else:
+        print 'must specify week number or date'
+        return
+
+    ls_table = [(1980,1,1,1981,7,1),\
               (1981,7,1,1982,7,1),\
               (1982,7,1,1983,7,1),\
               (1983,7,1,1985,7,1),\
@@ -20,14 +34,14 @@ def convert_to_utc(wk, sow):
               (2012,7,1,2015,7,1),\
               (2015,7,1,2017,1,1)]
 
-  leap_seconds = 0
-  for entry in ls_table:
+    leap_seconds = 0
+    for entry in ls_table:
       if dt >= datetime.datetime(entry[0],entry[1],entry[2]) and dt < datetime.datetime(entry[3],entry[4],entry[5]):
           break
       else:
           leap_seconds = leap_seconds + 1
 
-  sow = sow - leap_seconds
-  dt = dt + datetime.timedelta(seconds=sow)
+    sow = sow - leap_seconds
+    dt = dt + datetime.timedelta(seconds=sow)
 
-  return dt
+    return dt
